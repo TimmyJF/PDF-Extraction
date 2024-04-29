291ac632a39e5ef5259d8fe4277c52811f1e6bea
Timmy Feng <timmyjfeng@gmail.com>
2024-04-29 01:04:53 -0500

    Here are my most recent edits. The FastAPI is now working except make sure that you input the correct address in the correct format!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from extract_pdf import extract_from_folder
+import uvicorn
 
 # This is an API using FastAPI that will make it easier for the user to query data for the file that they are looking at
 # Initialize the app
@@ -14,6 +15,9 @@ def root():
     return {"message": "Welcome to the PDF extraction API!"}
 
 # Make an endpoint to add the folder path to your API
+# MAKE SURE THAT YOUR ADDRESS THAT YOU ARE POSTING IS FORMATTED CORRECTELY:
+# For example, my folder path is c:/Users/timmy/Dropbox/My PC (Timmy)/Downloads/k1x assignment
+# but I used c%3A%2FUsers%2Ftimmy%2FDropbox%2FMy%20PC%20%28Timmy%29%2FDownloads%2Fk1x%20assignment instead
 @app.post("/extract")
 def create_path(path: str):
     # Add the path to the paths list
@@ -21,15 +25,15 @@ def create_path(path: str):
     return paths
 
 # Make another endpoint for the API but at the folder path and call the extract_sum method
-@app.get("/extract/{folder_path}")
-def extract_sum(folder_path: int) -> str:
-    #try to call the extract_from_folder function. If not, then throw an exception
-    try:
-        total = extract_from_folder(paths[folder_path])
-        return {"total": total}
-    except Exception as e:
-        return {"Error": str(e)}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/extract/{index}")
+def extract_sum(index: int) -> dict:
+    # Check to see if the index is valid or not
+    if index < len(paths):
+        # Try to call the extract_from_folder function. If not, then throw an exception
+        try:
+            total = extract_from_folder(paths[index])
+            return {"total": total}
+        except Exception as e:
+            return {"Error": "Invalid folder path"}
+    else:
+        raise HTTPException(status_code=404, detail=f"Path {index} not found")
